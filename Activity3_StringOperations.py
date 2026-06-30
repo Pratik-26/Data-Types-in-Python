d55de96e265eb558855f78b508fb139f62269c59
Pratik maru <56751521+Pratik-26@users.noreply.github.com>
2026-06-30 15:46:56 +0530

    Update Activity3_StringOperations.py

---
+++ Activity3_StringOperations.py
@@ -0,0 +1,15 @@
+# ----------------------------------------------------
+# Activity 3 : String Operations
+# ----------------------------------------------------
+
+# Taking user input
+text = input("Enter a word or sentence: ")
+
+# Reverse the string
+revText = text[::-1]
+
+# Store reversed string
+text = revText
+
+print("\nReverse of the given string is:")
+print(text)
