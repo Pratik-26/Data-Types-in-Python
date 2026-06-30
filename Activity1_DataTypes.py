a586c8494ce1ada1b48548afd3cf0a2ebdc499a6
Pratik maru <56751521+Pratik-26@users.noreply.github.com>
2026-06-30 15:46:26 +0530

    Update Activity1_DataTypes.py

---
+++ Activity1_DataTypes.py
@@ -0,0 +1,29 @@
+# ----------------------------------------------------
+# Activity 1 : Data Types in Python
+# ----------------------------------------------------
+
+# Integer Data Type
+a = 5
+print("Value of a:", a)
+print("Data type of a:", type(a))
+
+print()
+
+# Float Data Type
+b = 2.5
+print("Value of b:", b)
+print("Data type of b:", type(b))
+
+print()
+
+# String Data Type
+c = "Codingal"
+print("Value of c:", c)
+print("Data type of c:", type(c))
+
+print()
+
+# Boolean Data Type
+d = True
+print("Value of d:", d)
+print("Data type of d:", type(d))
